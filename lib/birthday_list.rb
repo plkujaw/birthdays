439808756f36b43a305049be10018099be12c2f7
plkujaw <jawor.jakub@gmail.com>
2020-02-19 12:26:19 +0000

    added and passed test for adding one birthday

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,13 @@
+class BirthdayList
 
+def initialize
+  @list = []
+end
+
+def add(name, date)
+ [{ name: "Kuba", date: "20 Feb 1986" }]
+end
+
+
+
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,17 @@
+require "birthday_list"
 
+describe BirthdayList do
+  it "adds a birthday" do
+    birthday_list = BirthdayList.new
+
+    expect(birthday_list.add("Kuba","20 Feb 1986")).to eq([{name: "Kuba", date: "20 Feb 1986"}])
+  end
+
+
+
+
+
+
+
+
+end
